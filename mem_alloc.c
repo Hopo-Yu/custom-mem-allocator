707fff64b747bf14a83667c97f6b03474194d1a2
root <root@DESKTOP-84ROMMI.localdomain>
2023-07-22 15:06:51 +0800

    coalesce and thread-safe

--- mem_alloc.c
+++ mem_alloc.c
@@ -1,13 +1,15 @@
-#include <unistd.h>  // For sbrk()
-#include "mem_alloc.h"
 #include <assert.h>  // for assert()
-#include <string.h>  // for memset() and memcpy()
-
+#include <unistd.h>  // For sbrk()
+#include <string.h>  // For memset()
+#include "mem_alloc.h"
 
 Block* base = NULL;
 Block* limit = NULL;
 
+pthread_mutex_t malloc_lock = PTHREAD_MUTEX_INITIALIZER;
+
 Block* expand_heap(size_t size) {
+    pthread_mutex_lock(&malloc_lock);
     Block* block;
     block = sbrk(0);
     void* request = sbrk(size + sizeof(Block));
@@ -26,6 +28,7 @@ Block* expand_heap(size_t size) {
             base->next = block;
         }
         base = block;
+        pthread_mutex_unlock(&malloc_lock);
         return block;
     }
 }
@@ -37,9 +40,11 @@ void* my_malloc(size_t size) {
     }
 
     // First time initialization
+    pthread_mutex_lock(&malloc_lock);
     if (!base) {
         block = expand_heap(size);
         if (!block) {
+            pthread_mutex_unlock(&malloc_lock);
             return NULL;
         }
         else {
@@ -48,24 +53,20 @@ void* my_malloc(size_t size) {
         }
     }
     else {
-        block = limit;
-        Block* last = block;
-        while (block && !(block->free && block->size >= size)) {
-            last = block;
-            block = block->next;
-        }
+        block = find_and_remove_best_fit(size);
         if (!block) {
             block = expand_heap(size);
             if (!block) {
+                pthread_mutex_unlock(&malloc_lock);
                 return NULL;
             }
-            last->next = block;
         }
         else {
             block->free = 0;
             block->magic = MAGIC;
         }
     }
+    pthread_mutex_unlock(&malloc_lock);
     return(block + 1);
 }
 
@@ -73,12 +74,16 @@ void my_free(void* ptr) {
     if (!ptr) {
         return;
     }
+    pthread_mutex_lock(&malloc_lock);
     Block* block;
     block = (Block*)ptr - 1;
     if (block->magic != MAGIC) {
+        pthread_mutex_unlock(&malloc_lock);
         return;
     }
     block->free = 1;
+    coalesce();
+    pthread_mutex_unlock(&malloc_lock);
 }
 
 void* my_calloc(size_t nmemb, size_t size) {
@@ -95,21 +100,71 @@ void* my_realloc(void* ptr, size_t size) {
     if (!ptr || size <= 0) {
         return NULL;
     }
+    pthread_mutex_lock(&malloc_lock);
     Block* block = (Block*)ptr - 1;
     if (block->magic != MAGIC) {
+        pthread_mutex_unlock(&malloc_lock);
         return NULL;
     }
     if (block->size >= size) {
+        pthread_mutex_unlock(&malloc_lock);
         return ptr;
     }
     else {
         void* new_ptr;
         new_ptr = my_malloc(size);
         if (!new_ptr) {
+            pthread_mutex_unlock(&malloc_lock);
             return NULL;
         }
         memcpy(new_ptr, ptr, block->size);
         my_free(ptr);
+        pthread_mutex_unlock(&malloc_lock);
         return new_ptr;
     }
+}
+
+void coalesce() {
+    Block* current = base;
+    while (current && current->next) {
+        if (current->free && current->
+            next->free) {
+            current->size += current->next->size + sizeof(Block);
+            current->next = current->next->next;
+        }
+        else {
+            current = current->next;
+        }
+    }
+}
+
+Block* find_best_fit(size_t size) {
+    Block* current = base;
+    Block* best_fit = NULL;
+    while (current) {
+        if (current->free && current->size >= size &&
+            (!best_fit || current->size < best_fit->size)) {
+            best_fit = current;
+        }
+        current = current->next;
+    }
+    return best_fit;
+}
+
+Block* find_and_remove_best_fit(size_t size) {
+    Block* best_fit = find_best_fit(size);
+    if (best_fit) {
+        best_fit->free = 0;
+        // Split block if enough space
+        if (best_fit->size >= size + sizeof(Block) + 1) {
+            Block* new_block = (Block*)((char*)(best_fit + 1) + size);
+            new_block->size = best_fit->size - size - sizeof(Block);
+            new_block->next = best_fit->next;
+            new_block->free = 1;
+            new_block->magic = MAGIC;
+            best_fit->size = size;
+            best_fit->next = new_block;
+        }
+    }
+    return best_fit;
 }
--- mem_alloc.h
+++ mem_alloc.h
@@ -2,6 +2,7 @@
 #define MEM_ALLOC_H
 
 #include <stddef.h>  // For size_t
+#include <pthread.h> // For mutex
 
 #define MAGIC 0x12345678
 
@@ -16,5 +17,8 @@ void* my_malloc(size_t size);
 void* my_calloc(size_t nmemb, size_t size);
 void* my_realloc(void* ptr, size_t size);
 void my_free(void* ptr);
+void coalesce();
+Block* find_best_fit(size_t size);
+Block* find_and_remove_best_fit(size_t size);
 
 #endif  // MEM_ALLOC_H
